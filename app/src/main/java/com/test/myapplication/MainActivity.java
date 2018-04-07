aa5c4e5bfea2fb495e100a1d7f9998bb1e69aa76
Yanjing <yanjingtp@gmail.com>
2018-04-07 16:40:01 +0800

    Android 徽章显示

---
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.test.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.tv = (TextView) findViewById(R.id.tv);
+
+        BadgeView badgeView = new BadgeView(this, tv);  //初始化徽章
+
+        badgeView.setText("10");  //徽章内容
+        badgeView.setBadgePosition(BadgeView.POSITION_TOP_LEFT);  //徽章位置，默认右上角
+        badgeView.show();  //显示
+
+    }
+
+}
